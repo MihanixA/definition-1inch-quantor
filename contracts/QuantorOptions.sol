4c006ed237cc60b1e75bb12d1ced5edeeb1c32ba
Mikhail S <mihanix.it@gmail.com>
2022-02-20 15:38:33 +0300

    interim

--- contracts/QuantorOptions.sol
+++ contracts/QuantorOptions.sol
@@ -15,11 +15,14 @@ import "./interfaces/IQuantorGovernance.sol";
 
 
 contract QuantorOptions is IQuantorOptions, ERC721, ERC721Burnable {
+    using SafeERC20 for ERC20;
 
     IQuantorGovernance public quantorGovernance;
     ILimitOrderProtocol public limitOrderProtocol;
-    mapping(bytes32 => uint256) public optionConfigHashToNftId;
-    mapping(bytes32 => uint256) public limitOrderHashToNftId;
+    mapping(uint256 => bytes32) public optionConfigHashToNftId;
+    mapping(uint256 => bytes32) public limitOrderHashToNftId;
+
+    uint256 private _topNft = 1;
 
     constructor(address quantorGovernance_, address limitOrderProtocol_) ERC721("QOPTS", "Quantor Options Protocol") {
         quantorGovernance = IQuantorGovernance(quantorGovernance_);
@@ -29,19 +32,26 @@ contract QuantorOptions is IQuantorOptions, ERC721, ERC721Burnable {
     function mintOption(OptionConfig memory optionConfig) external {
         require(quantorGovernance.isWhitelistedAsset(optionConfig.makerAssetAddress));
         require(quantorGovernance.isWhitelistedAsset(optionConfig.takerAssetAddress));
+        IERC20(optionConfig.makerAssetAddress).safeTransferFrom(msg.sender, address(this), optionConfig.makerAmount);
         ILimitOrderProtocol.Order memory order;
         order.salt = optionConfig.salt;
         order.makerAsset = optionConfig.makerAssetAddress;
         order.takerAsset = optionConfig.takerAssetAddress;
-        order.maker = msg.sender;
+        order.maker = address(this);
+        order.allowedSender = address(this);
+        order.makingAmount = optionConfig.makerAmount;
+        order.takingAmount = 0;
         order.predicate = "";
         order.permit = "";
         order.interaction = "";
         limitOrderProtocol.fillOrderTo(order, "", optionConfig.makerAmount, 0, optionConfig.takerAmount, address(this));
+        _safeMint(msg.sender, _topNft);
+        optionConfigHashToNftId[_topNft] = hashOptionConfig(optionConfig);
+        limitOrderHashToNftId[_topNft] = limitOrderProtocol.hashOrder(order);
     }
 
     function burnOption(OptionConfig memory optionConfig) external {
-
+        // TODO
     }
 
     function hashOptionConfig(OptionConfig memory optionConfig) public pure returns (bytes32) {
